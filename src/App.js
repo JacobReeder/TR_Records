b4ae917a8f5cb295dafa6b0a7c141d6b498afbbb
JacobReeder <jacob.reeder1@gmail.com>
2023-01-22 12:50:31 -0800

    Added About section and Image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import About from './components/About';
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
+      <main>
+        <About>
+        
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://transylvaniantapes.bandcamp.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Transylvanian Recordings
         </a>
-      </header>
+        </About>
+      </main>
     </div>
   );
 }
